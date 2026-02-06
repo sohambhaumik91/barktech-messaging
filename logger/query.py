4a2f7143d3e40ebac2f5ef33908c8f61c6262783
sohambhaumik91 <sohambhaumik91@gmail.com>
2026-02-06 22:42:58 +0530

    updated gitignore file

--- logger/query.py
+++ logger/query.py
@@ -3,6 +3,7 @@ DB_PATH = "events.db"
 conn = sqlite3.connect(DB_PATH, check_same_thread=False)
 cursor = conn.cursor()
 
-cursor.execute("CREATE INDEX idx_events_device_type_time on events(device_id, event_type, timestamp_ms)")
+# cursor.execute("CREATE INDEX idx_events_device_type_time on events(device_id, event_type, timestamp_ms)")
 # cursor.execute("DROP INDEX idx_events_device_type_time")
+cursor.execute("DROP TABLE events")
 conn.commit()
